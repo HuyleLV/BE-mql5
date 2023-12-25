398c88d82f92623c6d1b777f5b1ef3a3491b0124
unknown <minhuy10a2@gmail.com>
2023-12-25 11:54:17 +0700

    fix api

--- src/model/category.js
+++ src/model/category.js
@@ -10,46 +10,33 @@ const Category = (category) => {
 };
 
 Category.getAll = (callback) => {
-  let arrChild = [];
-  let arr = [];
   const sqlString = `SELECT * FROM category ORDER BY category_id DESC`;
-  const sqlStringChild = `SELECT * FROM categorychild WHERE category_id =?`;
   db.query(sqlString, (err, result) => {
     if (err) {
-      return callback(err);
-    }
-
-    for (let i = 0; i < result.length; i++) {
-      db.query(sqlStringChild, result[i].category_id,(err1, result1) => {
-        if (err1) {
-          return callback(err1);
-        }
-        for (let j = 0; j < result1.length; j++) {
-          arrChild.push({
-            categoryChild_id: result1[j].categoryChild_id,
-            categoryChild_name: result1[j].categoryChild_name,
-            categoryChild_description: result1[j].categoryChild_description,
-            category_id: result1[j].category_id,
-            create_at: result1[j].create_at,
-            created_by: result1[j].created_by,
+      callback(err);
+    } else {
+      const promises = result.map((category) => {
+        return new Promise((resolveChild, rejectChild) => {
+          const sqlStringChild = `SELECT * FROM categorychild WHERE category_id = ?`;
+          db.query(sqlStringChild, category.category_id, (err1, result1) => {
+            if (err1) {
+              rejectChild(err1);
+            } else {
+              category.categoryChild = result1;
+              resolveChild();
+            }
           });
-      }
-        
-        arr.push({
-          category_id: result[i].category_id,
-          category_name: result[i].category_name,
-          category_description: result[i].category_description,
-          category_link: result[i].category_link,
-          create_at: result[i].create_at,
-          create_by: result[i].create_by,
-          categoryChild: arrChild
         });
-        console.log(arr);
       });
-    };
 
-    callback(arr);
-
+      Promise.all(promises)
+          .then(() => {
+            callback(result);
+          })
+          .catch((error) => {
+            callback(error);
+          });
+    }
   });
 }
 
